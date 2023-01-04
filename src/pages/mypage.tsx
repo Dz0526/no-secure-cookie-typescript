702b3f2b5930d0def87059222c922dac18f42d42
Dz0526 <this@dz99.me>
2023-01-04 17:53:25 +0900

    Feat(page): complete

---
+++ src/pages/mypage.tsx
@@ -0,0 +1,12 @@
+const MyPage = () => {
+  return (
+    <div className='flex-cover'>
+      <div className='mypage-card'>
+        <h3>マイページ</h3>
+        <p>a さんでログインしています</p>
+      </div>
+    </div>
+  );
+};
+
+export default MyPage;
